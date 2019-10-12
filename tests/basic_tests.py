9a9ee70ffcfa1b695cea18a019f7d510c14e6b12
David Luevano <55825613+luevano@users.noreply.github.com>
2019-10-11 20:45:44 -0600

    Restructure files.

---
+++ tests/basic_tests.py
@@ -0,0 +1,32 @@
+# create_database()
+# update_database()
+# download_manga()
+
+# print(os.listdir())
+
+# chapter_csv_to_df(chapter_csv='kimetsu_no_yaiba.csv')
+# download_manga()
+
+# chapter_list = get_chapters_list()
+# for chapter in chapter_list:
+#     print(chapter)
+
+# chapters_list_to_csv(chapters_list=chapter_list)
+
+# os.chdir('data/kimetsu_no_yaiba')
+# for image in chapter_image_csv_to_list('chapter_1_cruelty.csv'):
+#    print(image)
+
+# chapters_list = chapters_csv_to_list(chapter_csv='kimetsu_no_yaiba.csv')
+
+# chapter_image_list_to_csv(chapters_list[0])
+
+
+# first_chapter_img_url_list = get_chapter_image_list(chapter_list[0])
+
+# download_image(first_chapter_img_url_list[0])
+
+
+# os.chdir(MANGA_DIR)
+
+# download_chapter(first_chapter_img_url_list)
--- tirante/tirante.py
+++ tirante/tirante.py
@@ -36,7 +36,7 @@ MANGA_NAME_URL = 'kimetsu_no_yaiba/'
 MANGA_DIR = 'E:\\Mangas\\'
 # PC main manga data location.
 MANGA_DATA_DIR = ''.join(['C:\\Users\\Lorentzeus\\Google Drive\\',
-                          'Personal\\Python\\mangas\\data'])
+                          'Personal\\Python\\mangas\\test_data'])
 
 
 def get_chapters_list(main_url=MAIN_URL,
@@ -443,44 +443,3 @@ def download_manga(manga_name=MANGA_NAME,
         else:
             print(''.join([chapter_name,
                            ' already downloaded.']))
-
-
-#
-#
-# TEST STUFF
-#
-
-# create_database()
-# update_database()
-# download_manga()
-
-# print(os.listdir())
-
-# chapter_csv_to_df(chapter_csv='kimetsu_no_yaiba.csv')
-# download_manga()
-
-# chapter_list = get_chapters_list()
-# for chapter in chapter_list:
-#     print(chapter)
-
-# chapters_list_to_csv(chapters_list=chapter_list)
-
-# os.chdir('data/kimetsu_no_yaiba')
-# for image in chapter_image_csv_to_list('chapter_1_cruelty.csv'):
-#    print(image)
-
-# chapters_list = chapters_csv_to_list(chapter_csv='kimetsu_no_yaiba.csv')
-
-# chapter_image_list_to_csv(chapters_list[0])
-
-
-# first_chapter_img_url_list = get_chapter_image_list(chapter_list[0])
-
-# download_image(first_chapter_img_url_list[0])
-
-
-# os.chdir(MANGA_DIR)
-
-# download_chapter(first_chapter_img_url_list)
-
-print('This is a test, bitch.')
