3b6420bfd00e9a7bc48246b0d9b32f90702e5352
David Luevano <55825613+luevano@users.noreply.github.com>
2019-10-12 00:43:52 -0600

    Add setup.py, update files

--- tirante/__init__.py
+++ tirante/__init__.py
@@ -20,7 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from version import __version__
+from __version__ import __version__
 from cr_dat import create_database
 from up_dat import update_database
 from down_man import download_manga
