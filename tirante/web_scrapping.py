0675903b26de47749b639b71e506790f63e46ec5
David Luevano <55825613+luevano@users.noreply.github.com>
2019-12-28 18:47:01 -0700

    Tidy up code

--- tirante/web_scrapping.py
+++ tirante/web_scrapping.py
@@ -20,11 +20,13 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
 import requests
 from bs4 import BeautifulSoup
 import re
 
 
+# Thanks to https://stackoverflow.com/a/27086669 for this solution.
 def del_multiple_chars(text, chars):
     """
     Deletes multiple characters in a text and returns the 'fixed' text.
@@ -77,3 +79,32 @@ def get_chapters_list(manga_url,
         return chapter_list[::-1]
     else:
         return chapter_list
+
+
+def get_chapter_image_list(chapter_data):
+    """
+    Gets the links for each image in the chapter,
+        and returns a list of the image urls and its file name.
+    chapter_data: a list containing a url and a title.
+    NOTE: Not for direct use with the result of 'get_chapters_list'
+    """
+    # Get the data from the html and parse it.
+    page = requests.get(chapter_data[0])
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Get the "vung-doc" class, this contains a url for each page,
+    # which redirects to the source of the image.
+    # Deletes the first and last items, since they're trash.
+    soup_img = soup.find_all('img')
+    del soup_img[0]
+    del soup_img[len(soup_img) - 1]
+
+    # Stores each image url in a list.
+    image_url_list = []
+    for img in soup_img:
+        # Gets the sring of the url, splits it by the char '/',
+        # and gets the last item, which is the name of the file.
+
+        image_url_list.append([img['src'], img['src'].split('/')[-1]])
+
+    return image_url_list
